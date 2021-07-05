1f8c57d71bd379020e6ef9a04e3ec8bc5d715afb
Lanka Rathnayake <LankaR@99x.io>
2021-07-05 20:46:39 +0530

    Display Blog

---
+++ client/src/Components/Admin/Blog/createblog.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BlogCrud from '../crud/blogCrud'
+
+
+
+
+export default function blog() {
+    return (
+        <div>
+            <p>My blog</p>
+            <BlogCrud />
+        </div>
+    )
+}
