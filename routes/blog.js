adbac61b8739b449c59b7d2b434dafecf2286152
Lanka Rathnayake <LankaR@99x.io>
2021-07-07 21:42:19 +0530

    Add the routes to blog api

--- routes/blog.js
+++ routes/blog.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router();
-const { create } = require('../controllers/blogController');
+const { create, list, listAllBlogsCategoriesTags, read, remove, update } = require('../controllers/blogController');
 const { userAuth, checkRoleAdmin } = require('../controllers/auth')
 
 router.post('/create', userAuth, create)
+router.get('/blogs', list)
+router.post('/blogs-categories-tags', listAllBlogsCategoriesTags);
+router.get('/blog/:slug', read);
+router.delete('/blog/:slug', userAuth, remove);
+router.put('/blig/:slug', userAuth, update);
 
 module.exports = router;
