6cd4a4d2b644e63818fd3d22e69d0d062800c8e2
Lanka Rathnayake <LankaR@99x.io>
2021-07-10 12:27:49 +0530

    Adding the read remove update blogs

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 const fs = require('fs');
 const { smartTrim } = require('../helpers/excerpttrim');
+const blog = require('../models/blog');
 
 exports.create = (req, res) => {
     console.log("blog create backend hit")
@@ -169,13 +170,112 @@ exports.listAllBlogsCategoriesTags = (req, res) => {
 };
 
 exports.read = (req, res) => {
-    //
+    const slug = req.params.slug.toLowerCase();
+    Blog.findOne({ slug })
+        .populate('categories', '_id name slug')
+        .populate('tags', '_id name slug')
+        .populate('postedBy', '_id name username')
+        .select('_id title body slug mtitle mdesc categories tags postedBy createdAt updatedAt')
+        .exec((err, dara) => {
+            if (err) {
+                return res.json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(data)
+        });
 };
 
+
 exports.remove = (req, res) => {
-    //
+    const slug = req.params.slug.toLowerCase();
+    Blog.findOneAndRemove({ slug }).exec((err, data) => {
+        if (err) {
+            return res.json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            message: 'Blog Deleted successfully'
+        });
+    })
 };
+
 
 exports.update = (req, res) => {
     //
+    const slug = req.params.slug.toLowerCase();
+
+    Blog.findOne({ slug }).exec((err, oldBlog) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        let form = new formidable.IncomingForm();
+        form.keepExtensions = true;
+
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                return res.status(400).json({
+                    error: 'Image could not upload'
+                });
+            }
+            let slugBeforeMerge = oldBlog.slug;
+            oldBlog = _.merge(oldBlog, fields);
+            oldBlog.slug = slugBeforeMerge;
+
+            const { body, desc, categories, tags } = fields;
+
+            if (body) {
+                oldBlog.excerpt = smartTrim(body, 320, ' ', ' ...');
+                oldBlog.desc = stripHtml(body.substring(0, 160));
+            }
+
+            if (categories) {
+                oldBlog.categories = categories.split(',');
+            }
+
+            if (tags) {
+                oldBlog.tags = tags.split(',');
+            }
+
+            if (files.photo) {
+                if (files.photo.size > 50000000) {
+                    return res.status(400).json({
+                        error: 'Image should be less then 1mb in size'
+                    });
+                }
+                blog.photo.data = fs.readFileSync(files.photo.path)
+                blog.photo.contentType = files.photo.type
+            }
+
+            oldBlog.save((err, result) => {
+                if (err) {
+                    return res.status(400).json({
+                        error: errorHandler(err)
+                    });
+                }
+                // result.photo = undefined;
+                res.json(result);
+
+            })
+        })
+
+    })
 };
+
+exports.photo = (req, res) => {
+    const slug = req.params.slug.toLowercase();
+    Blog.findOne({ slug })
+        .select('photo')
+        .exec((err, blog) => {
+            if (err || !blog) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.set('Content-Type', blog.photo.contentType);
+            return res.send(blog.photo.data)
+        })
+}
