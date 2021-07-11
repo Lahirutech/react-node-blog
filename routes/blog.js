6c58b3c82acb5f8dba478ee9d46122e17f38c467
Lanka Rathnayake <LankaR@99x.io>
2021-07-11 19:38:48 +0530

    Add the blog route

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const { create, list, listAllBlogsCategoriesTags, read, remove, update } = require('../controllers/blogController');
+const { create, list, listAllBlogsCategoriesTags, photo, read, remove, update } = require('../controllers/blogController');
 const { userAuth, checkRoleAdmin } = require('../controllers/auth')
 
 router.post('/create', userAuth, create)
@@ -8,6 +8,7 @@ router.get('/blogs', list)
 router.post('/blogs-categories-tags', listAllBlogsCategoriesTags);
 router.get('/blog/:slug', read);
 router.delete('/blog/:slug', userAuth, remove);
-router.put('/blig/:slug', userAuth, update);
+router.put('/blog/:slug', userAuth, update);
+router.get('/blog/photo/:slug', photo);
 
 module.exports = router;
