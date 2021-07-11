0d870fe364a73df3a5cb745f77112f4b0c363e3d
Lanka Rathnayake <LankaR@99x.io>
2021-07-11 19:39:45 +0530

    Add Blog servicesw

--- client/src/Services/blogService.js
+++ client/src/Services/blogService.js
@@ -1,3 +1,4 @@
+
 export default {
     createBlog: blog => {
         console.log("blogservice", blog.get('title'))
@@ -12,5 +13,70 @@ export default {
                 return response.json();
             })
             .catch(err => console.log(err));
+    },
+    getBlogsandCategories: (limit, skip) => {
+        const data = {
+            limit, skip
+        }
+        return fetch('/blog/blogs-categories-tags', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }, body: JSON.stringify(data)
+        })
+            .then(response => {
+                return response.json();
+            })
+            .catch(err => console.log(err));
+
+    },
+    singleblog: (slug) => {
+        return fetch(`/blog/${slug}`, {
+            method: 'GET'
+        }).then(response => {
+            return response.json();
+        })
+            .catch(err => console.log(err))
+    },
+    listAll: () => {
+        return fetch(`/blogs`, {
+            method: 'GET'
+        })
+            .then(response => {
+                return response.json();
+            })
+            .catch(err => console.log(err))
+    },
+    removeBlog: (slug) => {
+        return fetch(`/blog/${slug}`, {
+            method: 'DELETE',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => {
+                return response.json();
+            })
+            .catch(err => console.log(err))
+
+    },
+    updateBlog: (blog, slug) => {
+        return fetch(`/blog/${slug}`, {
+            method: 'PUT',
+            headers: {
+                Accept: 'application/json'
+            },
+            body: blog
+        }).then(response => {
+            return response.json();
+        })
+            .catch(err => console.log(err))
+    },
+
+    listSearch: (params) => {
+
     }
+
 }
